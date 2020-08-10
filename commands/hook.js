81260822a940646628f70129e029cbe44ee01c22
HoangNguyen95 <hoangnguyen91095@gmail.com>
2020-08-10 16:47:46 +0700

    Remove functions

--- commands/hook.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'hook',
-    description: '!Hook',
-    execute(message, args) {
-        function hook(channel, title, message, colour, URL) {
-            
-        }
-    }
-}
